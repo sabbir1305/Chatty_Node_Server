ff5e74a6ba9c3427be8288f018871c16e2ae3066
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-07-31 21:49:21 +0600

    ServerRoomMessageAdded

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ socket.emit('message',{user:'Admin',text:`${user.name}, welcome to the room ${us
 socket.broadcast.to(user.room).emit('message',{user: 'Admin',text:`${user.name}, has joined!`});
 socket.join(user.room);
 
+io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
 callback();
 
 });
@@ -35,12 +36,19 @@ callback();
 socket.on('sendMessage',(message,callback)=>{
 const user = getUser(socket.id);
 io.to(user.room).emit('message',{user:user.name,text:message});
+io.to(user.room).emit('roomData',{room:user.room,text:message});
 
 callback();
 });
 
 socket.on('disconnect',()=>{
 console.log('user left');
+const user = removeUser(socket.id);
+
+if(user){
+    io.to(user.room).emit('message',{user:'admin',text:`${user.name} has left`})
+}
+
 });
 });
 app.use(router);
